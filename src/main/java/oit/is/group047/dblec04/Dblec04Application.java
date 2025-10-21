bb418119e1e958fc8dc579d62c9dda6fe237fdcb
tkhs-0114 <tkhs.mail0114@gmail.com>
2025-10-21 13:56:23 +0900

    init project

---
+++ src/main/java/oit/is/group047/dblec04/Dblec04Application.java
@@ -0,0 +1,13 @@
+package oit.is.group047.dblec04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dblec04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dblec04Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/group047/dblec04/Dblec04ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.group047.dblec04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dblec04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
